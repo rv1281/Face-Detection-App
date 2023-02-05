5bdc9f9a4858bbc174b257edf173eb1f31eb5450
Rohit Verma <rohit.verma@iic.ac.in>
2023-02-05 19:53:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+print("Program is running.\n Press q to exit.")
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_gray = gray[y:y+w, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 1)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 1)
+
+    cv2.imshow('Face-Eye-Smile Detector', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+print("\n\n\n\n\nExiting.....")
+cap.release()
+cv2.destroyAllWindows()
